46049df664d7aa9f6da36952c3df5efe7516bc94
Miguel Costa <miguelpintodacosta@gmail.com>
2010-12-28 23:40:03 +0000

    packages alterados

--- Parque/src/baseDados/Query.java
+++ Parque/src/baseDados/Query.java
@@ -9,13 +9,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- *
+ * Aqui serve para colocar os metodos para charmar as query
  * @author Miguel
  */
 public class Query extends Model {
 
     public static ResultSet queryClientes () throws SQLException{
-        return Model.stmt.executeQuery("SELECT id_cliente FROM clientes");
+        return Model.stmt.executeQuery("SELECT * FROM clientes");
     }
 
 }
