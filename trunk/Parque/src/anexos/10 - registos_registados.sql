2319107c4ad716345a5a4da5680c51906d220eaa
miguelpintodacosta <miguelpintodacosta@b0c5b5d0-dd7a-77ba-524d-a46bfb1341b9>
2010-12-29 21:31:41 +0000

    sci.query alerado, sci.GestaoRelatorios alterado

---
+++ trunk/Parque/src/anexos/10 - registos_registados.sql
@@ -0,0 +1,101 @@
+
+INSERT INTO registos_registados VALUES ('1', to_date('2010-11-12 09:01:00','yyyy-mm-dd hh24:mi:ss'),to_date('2010-11-12 11:01:00','yyyy-mm-dd hh24:mi:ss'));
+
+INSERT INTO registos_registados VALUES ('2','2010-11-24 09:01:01','2010-11-24 12:01:01');
+
+INSERT INTO registos_registados VALUES ('3','2010-11-26 09:01:02','2010-11-26 13:01:02');
+
+INSERT INTO registos_registados VALUES ('4','2010-12-05 09:01:03','2010-12-05 10:01:03');
+
+INSERT INTO registos_registados VALUES ('5','2010-11-12 09:01:04','2010-11-12 15:01:04');
+
+INSERT INTO registos_registados VALUES ('6','2010-11-19 09:01:05','2010-11-19 18:01:05');
+
+INSERT INTO registos_registados VALUES ('7','2010-11-23 10:01:06','2010-11-23 13:01:06');
+
+INSERT INTO registos_registados VALUES ('8','2010-11-24 09:01:07','2010-11-24 12:01:07');
+
+INSERT INTO registos_registados VALUES ('9','2010-11-12 09:01:08','2010-11-12 12:01:08');
+
+INSERT INTO registos_registados VALUES ('10','2010-11-25 10:01:09','2010-11-25 12:01:09');
+
+INSERT INTO registos_registados VALUES ('11','2010-11-26 09:01:10','2010-11-26 12:01:10');
+
+INSERT INTO registos_registados VALUES ('12','2010-11-12 09:01:11','2010-11-12 13:01:11');
+
+INSERT INTO registos_registados VALUES ('13','2010-11-07 09:01:12','2010-11-07 10:01:12');
+
+INSERT INTO registos_registados VALUES ('14','2010-11-12 09:01:13','2010-11-12 11:01:13');
+
+INSERT INTO registos_registados VALUES ('15','2010-11-27 09:01:14','2010-11-27 11:01:14');
+
+INSERT INTO registos_registados VALUES ('16','2010-10-30 09:01:15','2010-10-30 12:01:15');
+
+INSERT INTO registos_registados VALUES ('17','2010-11-02 09:01:16','2010-11-02 12:01:16');
+
+INSERT INTO registos_registados VALUES ('18','2010-11-19 09:01:17','2010-11-19 11:01:17');
+
+INSERT INTO registos_registados VALUES ('19','2010-12-17 09:01:18','2010-12-17 14:01:18');
+
+INSERT INTO registos_registados VALUES ('20','2010-11-12 07:01:19','2010-11-12 09:01:19');
+
+INSERT INTO registos_registados VALUES ('21','2010-11-12 09:01:20','2010-11-12 12:01:20');
+
+INSERT INTO registos_registados VALUES ('22','2010-11-12 09:01:21','2010-11-12 13:01:21');
+
+INSERT INTO registos_registados VALUES ('23','2010-11-07 09:01:22','2010-11-07 12:01:22');
+
+INSERT INTO registos_registados VALUES ('24','2010-11-07 09:01:23','2010-11-07 19:01:23');
+
+INSERT INTO registos_registados VALUES ('25','2010-11-10 09:01:24','2010-11-10 13:01:24');
+
+INSERT INTO registos_registados VALUES ('26','2010-11-02 09:01:25','2010-11-02 12:01:25');
+
+INSERT INTO registos_registados VALUES ('27','2010-11-12 08:01:26','2010-11-12 10:01:26');
+
+INSERT INTO registos_registados VALUES ('28','2010-11-12 09:01:27','2010-11-12 13:01:27');
+
+INSERT INTO registos_registados VALUES ('29','2010-11-12 09:01:28','2010-11-12 13:01:28');
+
+INSERT INTO registos_registados VALUES ('30','2010-11-12 19:01:29','2010-11-12 21:01:29');
+
+INSERT INTO registos_registados VALUES ('31','2010-11-12 09:01:30','2010-11-12 12:01:30');
+
+INSERT INTO registos_registados VALUES ('32','2010-11-09 09:01:31','2010-11-09 14:01:31');
+
+INSERT INTO registos_registados VALUES ('33','2010-11-08 09:01:32','2010-11-08 15:01:32');
+
+INSERT INTO registos_registados VALUES ('34','2010-11-04 09:01:33','2010-11-04 16:01:33');
+
+INSERT INTO registos_registados VALUES ('35','2010-11-12 09:01:34','2010-11-12 09:01:34');
+
+INSERT INTO registos_registados VALUES ('36','2010-11-12 09:01:35','2010-11-12 09:01:35');
+
+INSERT INTO registos_registados VALUES ('37','2010-12-23 16:01:36','2010-12-23 16:01:36');
+
+INSERT INTO registos_registados VALUES ('38','2010-11-12 09:01:37','2010-11-12 12:01:37');
+
+INSERT INTO registos_registados VALUES ('39','2010-11-12 09:01:38','2010-11-12 12:01:38');
+
+INSERT INTO registos_registados VALUES ('40','2010-11-12 13:01:39','2010-11-12 15:01:39');
+
+INSERT INTO registos_registados VALUES ('41','2010-11-12 09:01:40','2010-11-12 12:01:40');
+
+INSERT INTO registos_registados VALUES ('42','2010-11-16 09:01:41','2010-11-16 12:01:41');
+
+INSERT INTO registos_registados VALUES ('43','2010-12-14 09:01:42','2010-12-14 14:01:42');
+
+INSERT INTO registos_registados VALUES ('44','2010-11-12 09:01:43','2010-11-12 14:01:43');
+
+INSERT INTO registos_registados VALUES ('45','2010-12-08 09:01:44','2010-12-08 15:01:44');
+
+INSERT INTO registos_registados VALUES ('46','2010-12-03 11:01:45','2010-12-03 16:01:45');
+
+INSERT INTO registos_registados VALUES ('47','2010-11-13 09:01:46','2010-11-13 19:01:46');
+
+INSERT INTO registos_registados VALUES ('48','2010-12-18 09:01:47','2010-12-18 13:01:47');
+
+INSERT INTO registos_registados VALUES ('49','2010-11-12 09:01:48','2010-11-12 12:01:48');
+
+INSERT INTO registos_registados VALUES ('50','2010-11-22 11:01:49','2010-11-22 14:01:49');
+
