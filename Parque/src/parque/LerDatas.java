cc62079b15eaaea351910d7bbf83e53b67d35c99
Miguel Costa <miguelpintodacosta@gmail.com>
2010-12-29 23:32:09 +0000

    ficheiros sql adicionados

--- Parque/src/parque/LerDatas.java
+++ Parque/src/parque/LerDatas.java
@@ -11,32 +11,32 @@ package parque;
  */
 public class LerDatas {
 
-public String getAno(String data){
+public static String getAno(String data){
     String ano = data.substring(0, 3);
     return ano;
    }
 
-public String getMes(String data){
+public static String getMes(String data){
     String mes = data.substring(5, 6);
     return mes;
    }
 
-public String getDia(String data){
+public static String getDia(String data){
     String dia = data.substring(8, 9);
     return dia;
    }
 
-public String getHora(String data){
+public static String getHora(String data){
     String hora = data.substring(11, 12);
     return hora;
    }
 
-public String getMin(String data){
+public static String getMin(String data){
     String min = data.substring(14, 15);
     return min;
    }
 
-public String getSec(String data){
+public static String getSec(String data){
     String sec = data.substring(17, 18);
     return sec;
    }
