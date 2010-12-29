a183b4a4e30c21cbc952fa9aa122d6c5f63b18ef
miguelpintodacosta <miguelpintodacosta@b0c5b5d0-dd7a-77ba-524d-a46bfb1341b9>
2010-12-29 11:44:12 +0000

    ficheiros sql adicionados

---
+++ trunk/Parque/src/sci/GestaoRelatorios.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sci;
+
+/**
+ *
+ * @author Miguel
+ */
+public class GestaoRelatorios {
+
+    public BaseDados _baseDados;
+
+    public String geraRelatorios(Object aPesqD) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void apresentaRelatorio(String aRelatorio) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ trunk/Parque/src/sci/Query.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sci;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Aqui serve para colocar os metodos para charmar as query
+ * @author Miguel
+ */
+public class Query extends Model {
+
+    /**
+     * Lista todos os clientes
+     * @return ResultSet
+     * @throws SQLException
+     */
+    public static ResultSet queryClientes () throws SQLException{
+        return Model.stmt.executeQuery("SELECT * FROM clientes");
+    }
+
+
+    /**
+     * Procura um cliente pelo ID e devolve a linha
+     * @param idCliente
+     * @return
+     * @throws SQLException
+     */
+    public static ResultSet procuraClientePorID(String sel) throws SQLException{
+        String sql = "SELECT * FROM clientes WHERE id_cliente ='";
+
+        ResultSet rSet = null;
+        rSet = Model.stmt.executeQuery(sql + sel + "'");
+
+        return rSet;
+    }
+
+}
