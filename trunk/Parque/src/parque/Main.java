35c7956e3c4cc54ab66cddfdd085b49d0b92b040
miguelpintodacosta <miguelpintodacosta@b0c5b5d0-dd7a-77ba-524d-a46bfb1341b9>
2010-12-29 23:48:48 +0000

    ficheiros sql adicionados

--- trunk/Parque/src/parque/Main.java
+++ trunk/Parque/src/parque/Main.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package parque;
 
 /**
@@ -15,7 +14,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
 
+        String data = "2010-01-14 09:14:10";
+
+        String ano = LerDatas.getAno(data);
+        String mes = LerDatas.getMes(data);
+        String dia = LerDatas.getDia(data);
+        String hora = LerDatas.getHora(data);
+        String min = LerDatas.getMin(data);
+        String sec = LerDatas.getSec(data);
+        
+        System.out.println(ano);
+        System.out.println(mes);
+        System.out.println(dia);
+        System.out.println(hora);
+        System.out.println(min);
+        System.out.println(sec);
+    }
 }
