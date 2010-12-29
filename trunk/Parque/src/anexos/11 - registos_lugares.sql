2a8951aa77ebe0ed3259a2b24d651eafd7a763b0
miguelpintodacosta <miguelpintodacosta@b0c5b5d0-dd7a-77ba-524d-a46bfb1341b9>
2010-12-29 21:58:35 +0000

    ficheiros sql adicionados

---
+++ trunk/Parque/src/anexos/11 - registos_lugares.sql
@@ -0,0 +1,173 @@
+
+INSERT INTO registos_lugares VALUES ('101',to_date('2010-12-03 12:00','yyyy-mm-dd hh24:mi:ss'),'');
+
+INSERT INTO registos_lugares VALUES ('102','2010-12-03 12:01','');
+
+INSERT INTO registos_lugares VALUES ('103','2010-12-03 12:02','');
+
+INSERT INTO registos_lugares VALUES ('104','2010-12-03 12:03','');
+
+INSERT INTO registos_lugares VALUES ('105','2010-12-03 12:04','');
+
+INSERT INTO registos_lugares VALUES ('106','2010-12-03 12:05','');
+
+INSERT INTO registos_lugares VALUES ('107','2010-12-03 12:06','');
+
+INSERT INTO registos_lugares VALUES ('108','2010-12-03 12:07','');
+
+INSERT INTO registos_lugares VALUES ('109','2010-12-03 12:08','');
+
+INSERT INTO registos_lugares VALUES ('110','2010-12-03 12:09','');
+
+INSERT INTO registos_lugares VALUES ('201','2010-12-03 12:10','');
+
+INSERT INTO registos_lugares VALUES ('202','2010-12-03 12:11','');
+
+INSERT INTO registos_lugares VALUES ('203','2010-12-03 12:12','');
+
+INSERT INTO registos_lugares VALUES ('204','2010-12-03 12:13','');
+
+INSERT INTO registos_lugares VALUES ('205','2010-12-03 12:14','');
+
+INSERT INTO registos_lugares VALUES ('206','2010-12-03 12:15','');
+
+INSERT INTO registos_lugares VALUES ('207','2010-12-03 12:16','');
+
+INSERT INTO registos_lugares VALUES ('208','2010-12-03 12:17','');
+
+INSERT INTO registos_lugares VALUES ('209','2010-12-03 12:18','');
+
+INSERT INTO registos_lugares VALUES ('210','2010-12-03 12:19','');
+
+INSERT INTO registos_lugares VALUES ('301','2010-12-03 12:20','');
+
+INSERT INTO registos_lugares VALUES ('302','2010-12-03 12:21','');
+
+INSERT INTO registos_lugares VALUES ('303','2010-12-03 12:22','');
+
+INSERT INTO registos_lugares VALUES ('304','2010-12-03 12:23','');
+
+INSERT INTO registos_lugares VALUES ('305','2010-12-03 12:24','');
+
+INSERT INTO registos_lugares VALUES ('401','2010-12-03 12:25','');
+
+INSERT INTO registos_lugares VALUES ('402','2010-12-03 12:26','');
+
+INSERT INTO registos_lugares VALUES ('403','2010-12-03 12:27','');
+
+INSERT INTO registos_lugares VALUES ('404','2010-12-03 12:28','');
+
+INSERT INTO registos_lugares VALUES ('405','2010-12-03 12:29','');
+
+INSERT INTO registos_lugares VALUES ('501','2010-12-03 12:30','');
+
+INSERT INTO registos_lugares VALUES ('502','2010-12-03 12:31','');
+
+INSERT INTO registos_lugares VALUES ('503','2010-12-03 12:32','');
+
+INSERT INTO registos_lugares VALUES ('504','2010-12-03 12:33','');
+
+INSERT INTO registos_lugares VALUES ('505','2010-12-03 12:34','');
+
+INSERT INTO registos_lugares VALUES ('111',to_date('2010-12-03 12:40','yyyy-mm-dd hh24:mi:ss'),to_date('2010-12-03 14:00','yyyy-mm-dd hh24:mi:ss'));
+
+INSERT INTO registos_lugares VALUES ('112','2010-12-03 12:41','2010-12-03 14:01');
+
+INSERT INTO registos_lugares VALUES ('113','2010-12-03 12:42','2010-12-03 14:02');
+
+INSERT INTO registos_lugares VALUES ('114','2010-12-03 12:43','2010-12-03 14:03');
+
+INSERT INTO registos_lugares VALUES ('115','2010-12-03 12:44','2010-12-03 14:04');
+
+INSERT INTO registos_lugares VALUES ('116','2010-12-03 12:45','2010-12-03 14:05');
+
+INSERT INTO registos_lugares VALUES ('117','2010-12-03 12:46','2010-12-03 14:06');
+
+INSERT INTO registos_lugares VALUES ('118','2010-12-03 12:47','2010-12-03 14:07');
+
+INSERT INTO registos_lugares VALUES ('119','2010-12-03 12:48','2010-12-03 14:08');
+
+INSERT INTO registos_lugares VALUES ('120','2010-12-03 12:49','2010-12-03 14:09');
+
+INSERT INTO registos_lugares VALUES ('121','2010-12-03 12:50','2010-12-03 14:10');
+
+INSERT INTO registos_lugares VALUES ('122','2010-12-03 12:51','2010-12-03 14:11');
+
+INSERT INTO registos_lugares VALUES ('123','2010-12-03 12:52','2010-12-03 14:12');
+
+INSERT INTO registos_lugares VALUES ('124','2010-12-03 12:53','2010-12-03 14:13');
+
+INSERT INTO registos_lugares VALUES ('211','2010-12-03 12:54','2010-12-03 14:14');
+
+INSERT INTO registos_lugares VALUES ('212','2010-12-03 12:55','2010-12-03 14:15');
+
+INSERT INTO registos_lugares VALUES ('213','2010-12-03 12:56','2010-12-03 14:16');
+
+INSERT INTO registos_lugares VALUES ('214','2010-12-03 12:57','2010-12-03 14:17');
+
+INSERT INTO registos_lugares VALUES ('215','2010-12-03 12:58','2010-12-03 14:18');
+
+INSERT INTO registos_lugares VALUES ('216','2010-12-03 12:59','2010-12-03 14:19');
+
+INSERT INTO registos_lugares VALUES ('217','2010-12-03 13:01','2010-12-03 14:20');
+
+INSERT INTO registos_lugares VALUES ('218','2010-12-03 13:02','2010-12-03 14:21');
+
+INSERT INTO registos_lugares VALUES ('219','2010-12-03 13:03','2010-12-03 14:22');
+
+INSERT INTO registos_lugares VALUES ('220','2010-12-03 13:04','2010-12-03 14:23');
+
+INSERT INTO registos_lugares VALUES ('311','2010-12-03 13:05','2010-12-03 14:24');
+
+INSERT INTO registos_lugares VALUES ('312','2010-12-03 13:06','2010-12-03 14:25');
+
+INSERT INTO registos_lugares VALUES ('313','2010-12-03 13:07','2010-12-03 14:26');
+
+INSERT INTO registos_lugares VALUES ('314','2010-12-03 13:08','2010-12-03 14:27');
+
+INSERT INTO registos_lugares VALUES ('315','2010-12-03 13:09','2010-12-03 14:28');
+
+INSERT INTO registos_lugares VALUES ('316','2010-12-03 13:10','2010-12-03 14:29');
+
+INSERT INTO registos_lugares VALUES ('317','2010-12-03 13:11','2010-12-03 14:30');
+
+INSERT INTO registos_lugares VALUES ('318','2010-12-03 13:12','2010-12-03 14:31');
+
+INSERT INTO registos_lugares VALUES ('410','2010-12-03 13:13','2010-12-03 14:32');
+
+INSERT INTO registos_lugares VALUES ('411','2010-12-03 13:14','2010-12-03 14:33');
+
+INSERT INTO registos_lugares VALUES ('412','2010-12-03 13:15','2010-12-03 14:34');
+
+INSERT INTO registos_lugares VALUES ('413','2010-12-03 13:16','2010-12-03 14:35');
+
+INSERT INTO registos_lugares VALUES ('414','2010-12-03 13:17','2010-12-03 14:36');
+
+INSERT INTO registos_lugares VALUES ('415','2010-12-03 13:18','2010-12-03 14:37');
+
+INSERT INTO registos_lugares VALUES ('416','2010-12-03 13:19','2010-12-03 14:38');
+
+INSERT INTO registos_lugares VALUES ('417','2010-12-03 13:20','2010-12-03 14:39');
+
+INSERT INTO registos_lugares VALUES ('418','2010-12-03 13:21','2010-12-03 14:40');
+
+INSERT INTO registos_lugares VALUES ('419','2010-12-03 13:22','2010-12-03 14:41');
+
+INSERT INTO registos_lugares VALUES ('420','2010-12-03 13:23','2010-12-03 14:42');
+
+INSERT INTO registos_lugares VALUES ('510','2010-12-03 13:24','2010-12-03 14:43');
+
+INSERT INTO registos_lugares VALUES ('511','2010-12-03 13:25','2010-12-03 14:44');
+
+INSERT INTO registos_lugares VALUES ('512','2010-12-03 13:26','2010-12-03 14:45');
+
+INSERT INTO registos_lugares VALUES ('513','2010-12-03 13:27','2010-12-03 14:46');
+
+INSERT INTO registos_lugares VALUES ('514','2010-12-03 13:28','2010-12-03 14:47');
+
+INSERT INTO registos_lugares VALUES ('515','2010-12-03 13:29','2010-12-03 14:48');
+
+INSERT INTO registos_lugares VALUES ('516','2010-12-03 13:30','2010-12-03 14:49');
+
+INSERT INTO registos_lugares VALUES ('517','2010-12-03 13:31','2010-12-03 14:50');
+
