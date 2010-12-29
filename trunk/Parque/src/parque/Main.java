7d2e669e66d984dcd2fedfc37b7347b49e1680a1
miguelpintodacosta <miguelpintodacosta@b0c5b5d0-dd7a-77ba-524d-a46bfb1341b9>
2010-12-29 23:53:36 +0000

    ficheiros sql adicionados

--- trunk/Parque/src/parque/Main.java
+++ trunk/Parque/src/parque/Main.java
@@ -17,18 +17,18 @@ public class Main {
 
         String data = "2010-01-14 09:14:10";
 
-        String ano = LerDatas.getAno(data);
-        String mes = LerDatas.getMes(data);
-        String dia = LerDatas.getDia(data);
-        String hora = LerDatas.getHora(data);
-        String min = LerDatas.getMin(data);
-        String sec = LerDatas.getSec(data);
+        int ano = LerDatas.getAno(data);
+        int mes = LerDatas.getMes(data);
+        int dia = LerDatas.getDia(data);
+        int hora = LerDatas.getHora(data);
+        int min = LerDatas.getMin(data);
+        int sec = LerDatas.getSec(data);
         
-        System.out.println(ano);
-        System.out.println(mes);
-        System.out.println(dia);
-        System.out.println(hora);
-        System.out.println(min);
-        System.out.println(sec);
+        System.out.println(""+ano);
+        System.out.println(""+mes);
+        System.out.println(""+dia);
+        System.out.println(""+hora);
+        System.out.println(""+min);
+        System.out.println(""+sec);
     }
 }
