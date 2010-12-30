4a2f555460011d5fdbef0af72d9b81012f9c64f7
miguelpintodacosta <miguelpintodacosta@b0c5b5d0-dd7a-77ba-524d-a46bfb1341b9>
2010-12-30 00:09:22 +0000

    ficheiros sql adicionados

--- trunk/Parque/src/parque/LerDatas.java
+++ trunk/Parque/src/parque/LerDatas.java
@@ -2,8 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package parque;
+
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -11,35 +12,41 @@ package parque;
  */
 public class LerDatas {
 
-public static int getAno(String data){
-    String ano = data.substring(0, 4);
-    return Integer.parseInt(ano);
-   }
+    public static int getAno(String data) {
+        String ano = data.substring(0, 4);
+        return Integer.parseInt(ano);
+    }
 
-public static int getMes(String data){
-    String mes = data.substring(5, 7);
-    return Integer.parseInt(mes);
-   }
+    public static int getMes(String data) {
+        String mes = data.substring(5, 7);
+        return Integer.parseInt(mes);
+    }
 
-public static int getDia(String data){
-    String dia = data.substring(8, 10);
-    return Integer.parseInt(dia);
-   }
+    public static int getDia(String data) {
+        String dia = data.substring(8, 10);
+        return Integer.parseInt(dia);
+    }
 
-public static int getHora(String data){
-    String hora = data.substring(11, 13);
-    return Integer.parseInt(hora);
-   }
+    public static int getHora(String data) {
+        String hora = data.substring(11, 13);
+        return Integer.parseInt(hora);
+    }
 
-public static int getMin(String data){
-    String min = data.substring(14, 16);
-    return Integer.parseInt(min);
-   }
+    public static int getMin(String data) {
+        String min = data.substring(14, 16);
+        return Integer.parseInt(min);
+    }
 
-public static int getSec(String data){
-    String sec = data.substring(17, 19);
-    return Integer.parseInt(sec);
-   }
+    public static int getSec(String data) {
+        String sec = data.substring(17, 19);
+        return Integer.parseInt(sec);
+    }
 
+    public static long diferencaEntreDatas(GregorianCalendar daTa1, GregorianCalendar daTa2) {
 
+        long dt1 = daTa1.getTimeInMillis();
+        long dt2 = daTa2.getTimeInMillis();
+        long resultado = (long) ((dt2 - dt1) * 0.001);
+        return resultado;
+    }
 }
