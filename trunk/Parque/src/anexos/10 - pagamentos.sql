2b25fe95567e45c019c0fbe096b05159b6a8ee49
miguelpintodacosta <miguelpintodacosta@b0c5b5d0-dd7a-77ba-524d-a46bfb1341b9>
2010-12-31 11:54:11 +0000

    já altera e cria clientes

---
+++ trunk/Parque/src/anexos/10 - pagamentos.sql
@@ -0,0 +1,60 @@
+
+INSERT INTO pagamentos VALUES ('1', to_date('2010-12-31 12:45:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('2',to_date('2010-12-30 12:35:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('3',to_date('2010-12-30 12:20:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('4',to_date('2010-12-30 12:10:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('5',to_date('2010-12-30 12:30:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('6',to_date('2010-12-30 11:50:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('7',to_date('2010-12-30 12:30:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('8',to_date('2010-12-30 11:40:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('9',to_date('2010-12-30 10:10:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('10',to_date('2010-12-31 12:12:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('11',to_date('2010-12-30 12:40:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('12',to_date('2010-12-30 12:55:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('13',to_date('2010-12-30 12:13:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('14',to_date('2010-12-30 12:19:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('15',to_date('2010-12-30 12:48:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('16',to_date('2010-12-30 12:40:00','yyyy-mm-dd hh24:mi:ss'),'3');
+
+INSERT INTO pagamentos VALUES ('17',to_date('2010-12-30 13:00:00','yyyy-mm-dd hh24:mi:ss'),'2');
+
+INSERT INTO pagamentos VALUES ('18',to_date('2010-12-30 12:10:00','yyyy-mm-dd hh24:mi:ss'),'2');
+
+INSERT INTO pagamentos VALUES ('19',to_date('2010-12-31 10:07:00','yyyy-mm-dd hh24:mi:ss'),'2');
+
+INSERT INTO pagamentos VALUES ('20',to_date('2010-12-31 12:20:00','yyyy-mm-dd hh24:mi:ss'),'2');
+
+INSERT INTO pagamentos VALUES ('21',to_date('2010-12-30 12:10:00','yyyy-mm-dd hh24:mi:ss'),'2');
+
+INSERT INTO pagamentos VALUES ('22',to_date('2010-12-31 12:14:00','yyyy-mm-dd hh24:mi:ss'),'2');
+
+INSERT INTO pagamentos VALUES ('23',to_date('2010-12-30 12:07:00','yyyy-mm-dd hh24:mi:ss'),'2');
+
+INSERT INTO pagamentos VALUES ('24',to_date('2010-12-31 12:45:00','yyyy-mm-dd hh24:mi:ss'),'2');
+
+INSERT INTO pagamentos VALUES ('25',to_date('2010-12-31 12:40:00','yyyy-mm-dd hh24:mi:ss'),'2');
+
+INSERT INTO pagamentos VALUES ('26',to_date('2010-12-31 12:40:00','yyyy-mm-dd hh24:mi:ss'),'1');
+
+INSERT INTO pagamentos VALUES ('27',to_date('2010-12-31 12:20:00','yyyy-mm-dd hh24:mi:ss'),'1');
+
+INSERT INTO pagamentos VALUES ('28',to_date('2010-12-31 10:30:00','yyyy-mm-dd hh24:mi:ss'),'1');
+
+INSERT INTO pagamentos VALUES ('29',to_date('2010-12-31 12:20:00','yyyy-mm-dd hh24:mi:ss'),'1');
+
+INSERT INTO pagamentos VALUES ('30',to_date('2010-12-31 12:30:00','yyyy-mm-dd hh24:mi:ss'),'1');
---
+++ trunk/Parque/src/anexos/7 - modos_pagamentos.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO modos_pagamentos VALUES ('0','Nao especificado');
+INSERT INTO modos_pagamentos VALUES ('1','Dinheiro');
+INSERT INTO modos_pagamentos VALUES ('2','Cheque');
+INSERT INTO modos_pagamentos VALUES ('3','Multibanco');
